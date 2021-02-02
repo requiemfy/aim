c460037740ff183cca89d47bbb923cb8239a8244
valar-morghulis-bit <jasonramirez@student.laverdad.edu.ph>
2021-02-03 04:53:28 +0800

    fine

--- about/main.js
+++ about/main.js
@@ -81,7 +81,7 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Creative Minds", "Critical Thinkers", "Legendary Creatures", "Phenomenal Individuals"],
+    strings: ["Creative Minds", "Critical Thinkers"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
