419dfe4f2dbf19fe0d5bb3de03329738c25a75f5
valar-morghulis-bit <jasonramirez@student.laverdad.edu.ph>
2021-02-04 02:29:20 +0800

    fine

--- about/main.js
+++ about/main.js
@@ -122,4 +122,16 @@ $(document).ready(function () {
       }
     }
   });
+
+
+  // remove all animations on resize
+
+  let resizeTimer;
+  window.addEventListener("resize", () => {
+    document.body.classList.add("resize-animation-stopper");
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+      document.body.classList.remove("resize-animation-stopper");
+    }, 400);
+  });
 });
